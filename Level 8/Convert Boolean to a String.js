b22727aab9f40824767847db73ba2914a19e0300
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-07-19 09:59:10 -0400

    Create Convert Boolean to a String.js

---
+++ Level 8/Convert Boolean to a String.js
@@ -0,0 +1,16 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+
+// My Solution
+//check boolean value
+//return appropriate string
+//use ternary operator
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
