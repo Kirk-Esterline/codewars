b3c7e30e733edb27681918a5637b08aec3efcfd0
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-09-04 09:53:15 -0400

    Create Messi Goals Function.js

---
+++ Level 8/Messi Goals Function.js
@@ -0,0 +1,19 @@
+/*
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+
+*/
+
+
+// My Solution
+
+// add all three leagues together
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
