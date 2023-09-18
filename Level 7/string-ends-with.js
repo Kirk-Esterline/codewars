f879c63817040475ac18fbb6d1be421bf8fa8fcb
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-09-18 14:55:04 -0400

    created string-ends-with.js

---
+++ Level 7/string-ends-with.js
@@ -0,0 +1,12 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+*/
+
+
+// My Solution
+
+// return ends with
+
+function solution(str, ending){
+    return str.endsWith(ending)
+  }
