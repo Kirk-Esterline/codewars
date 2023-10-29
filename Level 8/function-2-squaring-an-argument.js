bc87e5f3856606fa97edd8bbafc6252436b58b56
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-10-29 14:45:12 -0400

    created function-2-squaring-an-argument-js

---
+++ Level 8/function-2-squaring-an-argument.js
@@ -0,0 +1,12 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+// My Solution
+
+// Write the "square"-function here
+
+function square(num) {
+    return Math.pow(num,2)
+  }
