a5d64379aac65177c012130d31b3ab77ebf451f3
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-10-29 09:42:29 -0400

    found a solution to jaden strings.  Used map() to change and return the 'word'

--- Level 7/jaden-casing-strings.js
+++ Level 7/jaden-casing-strings.js
@@ -22,7 +22,35 @@ String.prototype.toJadenCase = function () {
     let newWords = []
     words.forEach( word => {
       let chars = word.split('')
-      newWords.push(`${chars[0].toUpperCase()} ${chars.slice(1).join('')})
+      newWords.push(`${chars[0].toUpperCase()} ${chars.slice(1).join('')}`)
     })
     return newWords.join(' ')
   };
+
+  //  ********  Next attempt ******** This one finally worked although isn't that elegant
+
+  // figure out what the function is looking for
+// use this. to access the test string and split into an array
+// split each word into chars
+// word.map() char index 0 to uppercase : char
+// return joined arrays
+
+String.prototype.toJadenCase = function () {
+  let words = this.split(' ')
+  let newWords = []
+  words.forEach( word => {
+    let chars = word.split('').map((char, idx) => {
+      return idx === 0 ? char.toUpperCase() : char
+    })
+    newWords.push(chars.join(''))
+  })
+  return newWords.join(' ')
+};
+
+// Here is the elegant solution from the 'solutions'  I was too flumuxed by 'this.' that I couldn't see this elegant solution
+
+String.prototype.toJadenCase = function () { 
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
