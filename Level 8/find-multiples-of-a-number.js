d4a4ae619440f5778c68fb78058d4849503e08a4
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-11-05 09:23:13 -0500

    created find-multiples-of-a-number.js

---
+++ Level 8/find-multiples-of-a-number.js
@@ -0,0 +1,26 @@
+/*
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+*/
+
+// My Solution
+
+// init array
+// while loop 
+// initialize iteration variable = integer + 1
+// iterate % integer === 0 iterate push array
+
+function findMultiples(integer, limit) {
+    let arr = []
+    let iter = integer
+    while (iter <= limit) {
+      if (iter % integer === 0) {
+        arr.push(iter)
+      }
+      iter++
+    }
+    return arr
+  }
+  
