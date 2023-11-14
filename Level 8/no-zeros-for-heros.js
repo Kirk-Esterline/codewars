41b10746e74b6b76841f3299eb980e68419b9fef
kirk-esterline <kirk.esterline@gmail.com>
2023-11-14 03:19:48 -0500

    updated no-zeros-for-heros.js with some elegant solutions and additional thoughts.

--- Level 8/no-zeros-for-heros.js
+++ Level 8/no-zeros-for-heros.js
@@ -1,8 +1,14 @@
-// Not Complete yet
+// Had to check the solutions.  The track I was going down started returning NaN with negative numbers
+
+/*
+Remove only ending zeros
+*/
 
 // split number into array 
 // from end push if
 
+
+// Frist attempt that was not working
 function noBoringZeros(n) {
     let digits = Array.from(String(n), Number)
     let resultArr = []
@@ -15,3 +21,24 @@ function noBoringZeros(n) {
     }
     return Number(resultArr.join(''))
   }
+
+  // A very elegant solution dividing the number by 10 for as long as the number is a multiple of 10 and not 0
+
+  function noBoringZeros(n) {
+  while(n%10==0 && n!=0){n/=10;}
+  return n;
+}
+
+// Here is a solution a long the lines of what I was trying to do, but pops the zeros off the end and 'breaks' at the first number > 0.
+
+function noBoringZeros(x) {
+  let a = `${x}`.split('');
+  for(let i = a.length -1; i >= 0; i--){
+    if (a[i] == 0){
+        a.pop()
+    }else{
+        break
+    }
+  }
+return Number(a.join(''))
+}
