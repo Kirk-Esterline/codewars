7d8ae0b61ff3dd1356e7a556c531816aaf81267e
kirk-esterline <kirk.esterline@gmail.com>
2023-11-22 12:01:28 -0500

    created take-the-first-n-elements.js

---
+++ Level 8/take-the-first-n-elements.js
@@ -0,0 +1,11 @@
+/*
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+*/
+
+// My solution
+
+//splice 0,n
+
+function take(arr, n) {
+    return arr.splice(0,n)
+  }
