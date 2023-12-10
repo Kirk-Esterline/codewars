65a5cfdb2da5ff9c1bc86a66c4813f58e4f9720b
kirk-esterline <kirk.esterline@gmail.com>
2023-12-10 08:59:19 -0500

    created grasshopper-function-syntax-debugging.js

---
+++ Level 8/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,18 @@
+/*
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+*/
+
+// My Solution
+
+// Start
+
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+// Finish
+
+function main (verb, noun) {
+    return verb + noun
+  }
