d36fb74e45d0ee68f6159ddfc28fba4dfc4310be
kirk-esterline <kirk.esterline@gmail.com>
2024-01-05 08:07:29 -0500

    created color-ghost.js

---
+++ Level 8/color-ghost.js
@@ -0,0 +1,20 @@
+/* 
+Color Ghost
+
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"
+*/
+
+// My Solution
+
+var Ghost = function() {
+    this.colors = ['red', 'white', 'purple', 'yellow'];
+    this.color = this.colors[Math.floor(Math.random() * 4)]
+    return this.color
+  };
