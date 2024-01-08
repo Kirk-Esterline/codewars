53f5344e10c0ca5efbdd1bd643b55d7dde007f99
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-01-08 09:46:39 -0500

    does-my-list-include-this.js

---
+++ Level 8/does-my-list-include-this.js
@@ -0,0 +1,11 @@
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+// My Solution
+
+// includes method
+
+function include(arr, item){
+    return arr.includes(item)
+  }
